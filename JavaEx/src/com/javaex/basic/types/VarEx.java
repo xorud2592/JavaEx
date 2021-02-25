125e3816f92a923364a85b7d0e958916761a74d6
이태경 <xorud2592@gmail.com>
2021-02-25 16:09:32 +0900

    변수 선언과 초기화 조회

---
+++ JavaEx/src/com/javaex/basic/types/VarEx.java
@@ -0,0 +1,16 @@
+package com.javaex.basic.types;
+
+public class VarEx {
+
+	public static void main(String[] args) {
+		
+		int myAge = 27;
+		
+		System.out.println("내 나이는: " + myAge);
+		
+		int v1 = 10, v2 = 20, v3 = 30;
+		
+		v1 = v2 = v3 = 40;
+	}
+}
+ 
