1e55d791118bec5d962aa5b19c32ffed5431459c
이태경 <xorud2592@gmail.com>
2021-02-26 15:49:32 +0900

    콘솔출

---
+++ JavaEx/src/com/javaex/basic/ConsoleEx.java
@@ -0,0 +1,26 @@
+package com.javaex.basic;
+
+public class ConsoleEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		consoleOutputEx();
+
+	}
+
+	private static void consoleOutputEx() {
+		// TODO Auto-generated method stub
+		System.out.print("Hello ");
+		System.out.println("Java");
+		System.out.println("NewLine");
+		
+		System.out.println("Hello \nJava");
+		System.out.println("Hello \t\tJava");
+		System.out.println("Hello \"Java\"");
+		
+		
+		String filePath = "D:\\myfolder\\myfile.txt";
+		System.out.println(filePath);
+	}
+
+}
