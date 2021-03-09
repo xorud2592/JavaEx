b4e0ca66e1083ae65f4331682860628deabe8e86
xorud2592 <xorud2592@gmail.com>
2021-03-09 15:48:56 +0900

    File Class 연습

---
+++ JavaEx/src/com/javaex/io/FileClassEx.java
@@ -0,0 +1,54 @@
+package com.javaex.io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileClassEx {
+
+	private static String rootPath = System.getProperty("user.dir") + "\\files\\";
+
+	public static void main(String[] args) {
+		File root = new File(rootPath);
+
+		System.out.println(rootPath + ":" + root.exists());
+
+		if (!root.exists()) {
+			root.mkdirs();
+		}
+
+		printInfo(root);
+		System.out.println();
+		
+		File file = new File(rootPath + "\\myFile.txt");
+		System.out.println("-> myFile.txt 생성");
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				System.err.println("파일을 만들지 못했습니다!");
+			}
+		}
+		printInfo(root);
+		System.out.println();
+			
+		System.out.println("-> myFile.txt 삭제");
+		file.delete();
+		
+		printInfo(root);
+	}
+
+	private static void printInfo(File file) {
+		System.out.println("==============");
+
+		if (file.isDirectory()) {
+			System.out.println("Directory: " + file.getName());
+			
+			File[] files = file.listFiles();
+			
+			for(File f : files) {
+				System.out.print(f.isDirectory() ? "d " : "f ");
+				System.out.println(f.getName() + ":" + f.length());
+			}
+		}
+	}
+}
